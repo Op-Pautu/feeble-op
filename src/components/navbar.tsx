d1efb18d8e2605743bff11ecc0f0d92f0cf656d4
OP <106511969+Op-Pautu@users.noreply.github.com>
2025-12-30 19:48:46 +0530

    feat: initial styles and navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <nav className="mt-10 mx-60 bg-white rounded-[200px]">
+      <div className="p-5 flex items-center justify-between">
+        <h1>Navbar</h1>
+      </div>
+    </nav>
+  );
+};
